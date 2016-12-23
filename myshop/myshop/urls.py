c4b30a79275950e042f88d1e8bcb5ca2b206837d
ABC <gialuan1919@gmail.com>
2016-12-23 09:16:11 +0700

    internationalization

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -17,17 +17,20 @@ from django.contrib import admin
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+from django.utils.translation import gettext_lazy as _
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^cart/', include('cart.urls', namespace='cart')),
-    url(r'^orders/', include('orders.urls', namespace='orders')),
-    url(r'^paypal/', include('paypal.standard.ipn.urls')),
-    url(r'^payment/', include('payment.urls', namespace='payment')),
-    url(r'coupon/', include('coupons.urls', namespace='coupons')),
+    url(_(r'^cart/'), include('cart.urls', namespace='cart')),
+    url(_(r'^orders/'), include('orders.urls', namespace='orders')),
+    url(_(r'^paypal/'), include('paypal.standard.ipn.urls')),
+    url(_(r'^payment/'), include('payment.urls', namespace='payment')),
+    url(_(r'coupon/'), include('coupons.urls', namespace='coupons')),
+    url(_(r'^rosetta/'), include('rosetta.urls')),
     url(r'^', include('shop.urls',namespace='shop')),
 
-]
+)
 
 if settings.DEBUG:
 	urlpatterns+= static(settings.MEDIA_URL,
